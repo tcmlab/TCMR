494f2c9716af98b6dad373e39ccb2ccb9dd6295e
LJK <40190848+tcmlab@users.noreply.github.com>
2023-11-08 17:45:48 +0800

    Delete R/mahuang.R

--- R/mahuang.R
+++
@@ -1,9 +0,0 @@
-#' ma huang
-#'
-#' The datasets was from ETCM database.
-#' A data frame with 28 rows and 2 variables
-#' @source \url{http://www.tcmip.cn/ETCM/index.php/Home/Index/yc_details.html?id=212}
-#' @examples
-#' head(mahuang)
-#'
-"mahuang"
