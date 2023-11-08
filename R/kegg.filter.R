4ff1b5aadaeb662d3de7dbe401f5b8a87e4e6cf5
LJK <40190848+tcmlab@users.noreply.github.com>
2023-11-08 17:46:03 +0800

    Delete R/kegg.filter.R

--- R/kegg.filter.R
+++
@@ -1,9 +0,0 @@
-#' kegg.filter
-#'
-#' The results after KEGG/GO screening of pathways and genes
-#' A data frame with 8 rows and 9 variables
-#' @source example data from the dot_sankey function
-#' @examples
-#' head(kegg.filter)
-#'
-"kegg.filter"
